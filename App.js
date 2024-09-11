38f12012cc4d2cc6507f8dc4ee5c8dea85e84650
luc12 <danunike687@gmail.com>
2024-09-11 11:12:49 +0700

    Verson 1

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import Header from "./screen/Header";
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>App quan ly bida</Text>
-      <Header />
       <StatusBar style="auto" />
     </View>
   );
