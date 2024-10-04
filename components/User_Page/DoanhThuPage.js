3f1d387d16c7b94063823bc6b3fa5522c5232dcd
luc12 <danunike687@gmail.com>
2024-10-04 09:27:59 +0700

    dung keo commit nay

---
+++ components/User_Page/DoanhThuPage.js
@@ -0,0 +1,8 @@
+export default function DoanhThuPage() {
+  //Hàm request tới api status lấy về dữ liệu id,total,date, starttime,endtime,totaltime,order
+  return (
+    <View>
+      <FlatList />
+    </View>
+  );
+}
